62761a64681576f0590c6d5be20837eb65edbc65
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2025-03-12 00:25:53 +0000

    finish mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-
+  site: 'https://bespokefloorsanding.ie',
+  integrations: [sitemap()],
+  output: 'static',
 });
